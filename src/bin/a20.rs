2bc48de65c5f80e91199a2df069b397958fb0070
Eurvin Morman <eurvin@gmail.com>
2023-09-01 21:53:14 +0200

    project 1 level 1

--- src/bin/a20.rs
+++ src/bin/a20.rs
@@ -1,18 +1,18 @@
 // Topic: User input
 //
 // Requirements:
-// * Verify user input against pre-defined keywords
-// * The keywords represent possible power options for a computer:
+// * Verify user input against pre-defined States
+// * The States represent possible power options for a computer:
 //   * Off
 //   * Sleep
 //   * Reboot
 //   * Shutdown
 //   * Hibernate
-// * If the user enters one of the keywords, a message should be printed to
+// * If the user enters one of the States, a message should be printed to
 //   the console indicating which action will be taken
 //   * Example: If the user types in "shutdown" a message should display such
 //     as "shutting down"
-// * If the keyword entered does not exist, an appropriate error message
+// * If the State entered does not exist, an appropriate error message
 //   should be displayed
 //
 // Notes:
@@ -23,4 +23,60 @@
 // * The program should be case-insensitive (the user should be able to type
 //   Reboot, reboot, REBOOT, etc.)
 
-fn main() {}
+use std::io;
+
+enum State {
+    Off,
+    Sleep,
+    Reboot,
+    Shutdown,
+    Hibernate,
+}
+
+fn get_input() -> io::Result<String> {
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer)?;
+    Ok(buffer.trim().to_owned())
+}
+
+fn fmt_string(s: String) -> String {
+    let mut fmtd_s = s.chars();
+    match fmtd_s.next() {
+        Some(c) => c.to_uppercase().collect::<String>() + fmtd_s.as_str(),
+        None => String::new(),
+    }
+}
+
+fn handle_input(user_input: String) -> Result<State, ()> {
+    match user_input.as_str() {
+        "Off" => Ok(State::Off),
+        "Sleep" => Ok(State::Sleep),
+        "Reboot" => Ok(State::Reboot),
+        "Shutdown" => Ok(State::Shutdown),
+        "Hibernate" => Ok(State::Hibernate),
+        _ => Err(()),
+    }
+}
+
+fn print_state(state: State) {
+    match state {
+        State::Off => println!("Turning off!"),
+        State::Sleep => println!("Turning to sleep!"),
+        State::Reboot => println!("Rebooting!"),
+        State::Shutdown => println!("Shutting down!"),
+        State::Hibernate => println!("Hibernating!"),
+    }
+}
+
+fn main() {
+    let input_result = get_input();
+    let fmt_input = match input_result {
+        Ok(user_input) => fmt_string(user_input),
+        Err(e) => e.to_string(),
+    };
+    let get_state = handle_input(fmt_input);
+    let state = get_state;
+    if state.is_ok() {
+        print_state(state.unwrap());
+    }
+}
--- src/bin/p1.rs
+++ src/bin/p1.rs
@@ -18,4 +18,96 @@
 // * Create your program starting at level 1. Once finished, advance to the
 //   next level.
 
-fn main() {}
+use std::io;
+
+struct Bill {
+    name: String,
+    amount: i32,
+}
+
+struct Bills {
+    inner: Vec<Bill>,
+}
+
+impl Bills {
+    fn new() -> Self {
+        Self { inner: vec![] }
+    }
+
+    fn add(&mut self) -> () {
+        let new_bill = add_bill();
+        self.inner.push(new_bill);
+        println!("Your new bill has successfully been added!🎉");
+        match self.inner.last() {
+            None => println!("Something went wrong. No bills added!"),
+            Some(bill) => println!("Bill name: {} and amount: {}", bill.name, bill.amount),
+        }
+    }
+
+    fn view_all(&self) {
+        match self.inner.is_empty() {
+            true => println!("No bills available"),
+            false => {
+                for bill in &self.inner {
+                    println!("name: {} amount: {}", bill.name, bill.amount)
+                }
+            }
+        }
+    }
+}
+
+enum MenuOption {
+    Add,
+    View,
+    Invalid,
+    Exit,
+}
+
+// std::io operations
+fn get_input() -> String {
+    let mut buffer = String::new();
+    io::stdin()
+        .read_line(&mut buffer)
+        .expect("please enter a menu option");
+    buffer
+}
+
+// menu operations
+fn display_menu() {
+    println!("Please select your option:");
+    println!("1 - Add a new bill");
+    println!("2 - View bills");
+    println!("0 - Exit menu");
+}
+
+fn get_menu_choice() -> MenuOption {
+    let input = get_input();
+    match input.trim() {
+        "1" => MenuOption::Add,
+        "2" => MenuOption::View,
+        "0" => MenuOption::Exit,
+        _ => MenuOption::Invalid,
+    }
+}
+
+fn add_bill() -> Bill {
+    println!("Add a new bill!");
+    println!("Please add name:");
+    let name = get_input();
+    println!("Please add an amount:");
+    let amount = get_input().trim().parse::<i32>().unwrap();
+    Bill { name, amount }
+}
+
+fn main() {
+    let mut bills = Bills::new();
+    loop {
+        display_menu();
+        match get_menu_choice() {
+            MenuOption::Add => bills.add(),
+            MenuOption::View => bills.view_all(),
+            MenuOption::Invalid => println!("Please choose a valid menu option"),
+            MenuOption::Exit => break,
+        };
+    }
+}
