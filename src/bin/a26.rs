8080917a1cfd9140f6e3d95c8cb1f058dd9ee422
Eurvin Morman <eurvin@gmail.com>
2023-09-26 14:52:16 +0200

    exercises external crates

--- src/bin/a26.rs
+++ src/bin/a26.rs
@@ -8,4 +8,12 @@
 // * (OPTIONAL) Read the documentation section `Formatting and Parsing`
 //   for examples on how to create custom time formats
 
-fn main() {}
+use chrono::prelude::*;
+
+fn main() {
+    let lt = Local::now();
+    let dt = lt
+        .format_localized("%A %e %B %Y, %T", Locale::nl_NL)
+        .to_string();
+    println!("the current date and time are {:?}", dt)
+}
